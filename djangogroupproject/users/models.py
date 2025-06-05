2fe828cf2240dd6932d7dd04a2d28e2535cd4f64
Muhamad Zikri Bin Abd Haris <140304257+zikriharis@users.noreply.github.com>
2025-06-05 22:58:22 +0800

    Merge media applications into campaigns application and expand Users models.py

--- djangogroupproject/users/models.py
+++ djangogroupproject/users/models.py
@@ -1,12 +1,31 @@
+import uuid
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
-
 class User(AbstractUser):
     ROLE_CHOICES = (
         ('donor', 'Donor'),
-        ('campaigner', 'Campaigner'),
+        ('organization', 'Organization Main Account'),
+        ('manager','Campaign Manager'),
         ('admin', 'Admin'),
     )
+    id = models.URLField(primary_key=True, default=uuid.uuid4, editable=False)
     role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    country = models.CharField(max_length=50, blank=True, null=True)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    phone = models.CharField(max_length=30, blank=True)
+    bio = models.TextField(blank=True)
+
+class CampaignManagerAssignment (models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="manager_assignments")
+    campaign = models.ForeignKey('campaigns.Campaign', on_delete=models.CASCADE, related_name="managers")
+    organization = models.ForeignKey('organizations.Organization', on_delete=models.CASCADE)
+    added_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name="added_managers")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Meta:
+    unique_together = ('User', 'campaign')
